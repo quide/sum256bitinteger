57c59ab4a65048693fa18e99b290c9ed7a737353
quide <pedro_quide@hotmail.com>
2015-05-24 11:24:16 +0100

    added protections and better organization (+ object oriented)

--- sum256bit_ws/sum256/sum256bit.cpp
+++ sum256bit_ws/sum256/sum256bit.cpp
@@ -6,28 +6,6 @@
 #include <inttypes.h>
 
 
-// ASCII table converted to decimal integer
-#define X 0xff
-const unsigned char CHAR_TO_INT_GLOBAL[] =
-{
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, X, X, X, X, X, X,
-  X,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,
-  25,26,27,28,29,30,31,32,33,34,35,X, X, X, X, X,
-  X,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,
-  25,26,27,28,29,30,31,32,33,34,35,X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X,
-  X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X
-};
-
 void help(char** argv)
 {
 	printf("Usage: %s int_A int_B\n", argv[0]);
@@ -51,57 +29,115 @@ void help(char** argv)
   #define STATIC_ASSERT(e,m) \
     ;enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(!!(e)) }
 #endif
-void validate_64_bit_vars()
-{
-	uintmax_t teste = 0 ;
-	// Compile time error if we don't have 64 bit vars
-	STATIC_ASSERT(sizeof(teste)*8==64, "Compilador não suporta variáveis de 64 bits!");
-	//_Static_assert(sizeof(teste)*8==64, "Compilador não suporta variáveis de 64 bits!");
-	//static_assert(sizeof(teste)*8==64, "Compilador não suporta variáveis de 64 bits!");
-}
 
+// ASCII table converted to decimal integer
+// 'x' - is an out of scope big value, 120
+const unsigned char CHAR_TO_INT_GLOBAL[] =
+{
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+    0,   1,   2,   3,   4,   5,   6,   7,   8,   9, 'x', 'x', 'x', 'x', 'x', 'x',
+  'x',  10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,
+   25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35, 'x', 'x', 'x', 'x', 'x',
+  'x',  10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,
+   25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35, 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x',
+  'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'
+};
 
 #include <stdlib.h>
 
 #define NUMBER_OF_PARTS 4		// 4*64 bits = 256 bits
 #define MAX_BYTES_PER_PART 8 	// 64 bits = 8 bytes
+
 struct integer256
 {
+	const unsigned int MAX_HEX_INPUT_SIZE_PER_PART;
+	const unsigned int MAX_HEX_INPUT_SIZE;
 	uint64_t  parts[NUMBER_OF_PARTS];
 	bool overflow;
+	bool invalid;
     
     integer256() :
-		overflow(false)
+		MAX_HEX_INPUT_SIZE_PER_PART(MAX_BYTES_PER_PART*2),
+		MAX_HEX_INPUT_SIZE(NUMBER_OF_PARTS*MAX_HEX_INPUT_SIZE_PER_PART),
+		overflow(false),
+		invalid(false)
     {
-        for(int i = 0;  i < NUMBER_OF_PARTS;  ++i)
+        for(int i = 0;  i < NUMBER_OF_PARTS;  ++i)	// auto initialize to 0
         {	 parts[i] = 0;	 }
     }
+	
+	int init_from_string(char* varc1, unsigned int base)
+	{
+		size_t str1_size = strlen (varc1);
+		size_t i;
+		int c;
+		uint64_t temp ;
+		uint64_t dig;
+		const unsigned char *digit_value;
+		digit_value = CHAR_TO_INT_GLOBAL;
+		
+		int j=0;
+		while( str1_size > 0 ) // check if there is still something to be read for the next integer part
+		{
+			uint64_t dig1 = 0;	
+			for( i = 0;  i < str1_size && i < MAX_HEX_INPUT_SIZE_PER_PART;  ++i )	// Go thru all inserted letters until the max allowed per part (just read one 64 bit part at a time)
+			{
+				c = (unsigned char) *varc1++;				// Read next digit
+				dig = digit_value[c];						// convert from string (that may represent eg. hexadecimal) to decimal
+				if( dig >= base )							// (protect from invalid digits)
+				{
+					this->invalid = true;
+					return -1;	
+				}
+				temp = dig * pow(base, i);		// (this intermediate step avoids unkown +1 error that occured for base=16, i=14, dig=15 calcule added to dig1=0xFFFFFFFFFFFFFF !!!!!)
+				dig1 += temp;					// add this decimal to our 64 bit integer (a part) with its proper importance
+			}
+			str1_size = str1_size - i; 	// Reduce what was read from readed size
+			this->parts[j] = dig1;		// Add this part to our 256 bit integer
+			++j;
+		}
+		
+		return 0;
+	}
 	
 	integer256 operator+(const integer256& b)
 	{
 		integer256 result;
 		uint64_t carrier = 0;
 		
-		for(int i = 0;  i < NUMBER_OF_PARTS;  ++i)
+		if(b.invalid || this->invalid)
+		{	result.invalid = true;	}
+		else
 		{
-			result.parts[i] = this->parts[i] + b.parts[i] + carrier;
-			if(b.parts[i] > UINTMAX_MAX - carrier ||
-				b.parts[i] + carrier > UINTMAX_MAX - this->parts[i]) 	// Overflow case
-			{	carrier = 1;	}	// add +1 in the next sum
-			else
-			{	carrier = 0;	}
+			for(int i = 0;  i < NUMBER_OF_PARTS;  ++i)
+			{
+				result.parts[i] = this->parts[i] + b.parts[i] + carrier;	// do the actual sum
+				
+				if(b.parts[i] > UINTMAX_MAX - carrier ||
+					b.parts[i] + carrier > UINTMAX_MAX - this->parts[i]) 	// Overflow case
+				{	carrier = 1;	}	// add +1 in the next part sum
+				else
+				{	carrier = 0;	}
+			}
+			
+			if( carrier == 1 )	// Unhandlable Overflow
+			{	result.overflow = true;	}
 		}
-		
-		if( carrier == 1 )	// Unhandlable Overflow
-		{	result.overflow = true;	}
 		
 		return result;
 	}
 	
 	char* print()	// concatenate parts and form a string
 	{
-		const int MAX_HEX_INPUT_SIZE_PER_PART = MAX_BYTES_PER_PART*2;
-		const int MAX_HEX_INPUT_SIZE = NUMBER_OF_PARTS*MAX_HEX_INPUT_SIZE_PER_PART;
 		char* result = (char*)calloc(MAX_HEX_INPUT_SIZE+1, sizeof(char));
 
 		char* result_part[NUMBER_OF_PARTS];
@@ -116,7 +152,18 @@ struct integer256
 		if( this->overflow == true )
 		{	strcat(result, " (overflown)");	}
 		
+		if( this->invalid == true )
+		{	strcat(result, " Invalid value!");	}
+		
 		return result;
+	}
+	
+	void validate_64_bit_vars()		// Just for system independence validation (TODO)
+	{
+		uintmax_t teste = 0 ;
+		STATIC_ASSERT(sizeof(teste)*8==64, "Compilador não suporta variáveis de 64 bits!");				// Generate compile time error if we don't have 64 bit vars
+		//_Static_assert(sizeof(teste)*8==64, "Compilador não suporta variáveis de 64 bits!");
+		//static_assert(sizeof(teste)*8==64, "Compilador não suporta variáveis de 64 bits!");
 	}
 };
 
@@ -131,36 +178,6 @@ void tests()
 	printf("Min integer is %d bits with value %llx\n", sizeof(teste2)*8, INTMAX_MIN);
 	//uint64_t teste2 = 0;
 	//int64_t teste4 = 0;
-}
-
-int convert_string_to_256_bit_integer(integer256& full_dig1, char** argv, char* varc1, unsigned int base, size_t MAX_HEX_INPUT_SIZE_PER_PART)
-{
-	size_t str1_size = strlen (varc1);
-	size_t i;
-	int c;
-	uint64_t temp ;
-	uint64_t dig;
-	const unsigned char *digit_value;
-	digit_value = CHAR_TO_INT_GLOBAL;
-	
-	int j=0;
-	while( str1_size > 0 ) // check if there is still something to be read for the next integer part
-	{
-		uint64_t dig1 = 0;	
-		for( i = 0;  i < str1_size && i < MAX_HEX_INPUT_SIZE_PER_PART;  ++i )	// Go thru all inserted letters until the max allowed per part (just read one 64 bit part at a time)
-		{
-			c = (unsigned char) *varc1++;				// Read next digit
-			dig = digit_value[c];						// convert from string (that may represent eg. hexadecimal) to decimal
-			if( dig >= base ){help(argv); return -1;}	// (protect from invalid digits)
-			temp = dig * pow(base, i);		// this intermediate step avoids unkown +1 error that occured e.g. for base=16, i=14, dig=15 !!!!!
-			dig1 += temp;					// add this decimal to our 64 bit integer (a part) with its proper importance
-		}
-		str1_size = str1_size - i; 	// Reduce what was read from readed size
-		full_dig1.parts[j] = dig1;		// Add this part to our 256 bit integer
-		++j;
-	}
-	
-	return 0;
 }
 
 int main(int argc, char** argv)
@@ -178,28 +195,27 @@ int main(int argc, char** argv)
 	char* varc2	= argv[2];
 	size_t str2_size = strlen (varc2);
 	
-	const int MAX_HEX_INPUT_SIZE_PER_PART = MAX_BYTES_PER_PART*2;
-	const int MAX_HEX_INPUT_SIZE = NUMBER_OF_PARTS*MAX_HEX_INPUT_SIZE_PER_PART;
-	if(str1_size > MAX_HEX_INPUT_SIZE)
+	integer256 full_dig1;
+
+	if(str1_size > full_dig1.MAX_HEX_INPUT_SIZE)
 	{
 		printf("First hexadecimal integer must be 256 bits max (64 digits). It has %d!\n", str1_size);
 		help(argv); 
 		return -1;
 	}
-	if(str2_size > MAX_HEX_INPUT_SIZE)
+	if(str2_size > full_dig1.MAX_HEX_INPUT_SIZE)
 	{
 		printf("Second hexadecimal integer must be 256 bits max (64 digits). It has %d!\n", str2_size);
 		help(argv); 
 		return -1;
 	}
 
-	integer256 full_dig1;
-	convert_string_to_256_bit_integer(full_dig1, argv, varc1, 16, MAX_HEX_INPUT_SIZE_PER_PART);
+
+	full_dig1.init_from_string(varc1, 16);
 	
 	integer256 full_dig2;
-	convert_string_to_256_bit_integer(full_dig2, argv, varc2, 16, MAX_HEX_INPUT_SIZE_PER_PART);
+	full_dig2.init_from_string(varc2, 16);
 	
-
 	integer256  result = full_dig1 + full_dig2;
 	
 	//printf("result: %llx\n", result);
